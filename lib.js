7652c63866773b19ad370e7a899e6f0131f5f57b
AhmedAmin90 <49597460+AhmedAmin90@users.noreply.github.com>
2021-03-25 19:47:42 +0300

    Try with adding many books

--- lib.js
+++ lib.js
@@ -11,6 +11,7 @@ const boxSection = document.querySelector('#box-section')
 const boxDiv = document.createElement('div');
 const boxTitle = document.createElement('h1');
 const boxAuthor = document.createElement('h1');
+const boxPages = document.createElement('h1');
 const boxRead = document.createElement('p');
 
 
@@ -26,20 +27,27 @@ function Book(title, author , pages , read) {
 // book = new Book('test' , 'juan' , 500, true)
 function addBookToLib(){
   let newBook = new Book(bookTitle.value , bookAuthor.value , bookPages.value , bookRead.checked );
-  // console.log(newBook)
-}
-
-function createBookBox(){
-  boxSection.appendChild(boxDiv);
-  boxDiv.appendChild(boxTitle);
-  boxDiv.appendChild(boxAuthor);
-  boxDiv.appendChild(boxRead);
   boxTitle.innerText = bookTitle.value;
   boxAuthor.innerText = bookAuthor.value;
+  boxPages.innerText = bookPages.value;
   boxRead.innerText = bookRead.checked === true ? "I read it" : "Not really !"
+  myLibrary.push(newBook);
+
+}
+
+
+function createBookBox(){
+  for (let book of myLibrary) {
+ boxSection.appendChild(boxDiv);
+  boxDiv.appendChild(boxTitle);
+  boxDiv.appendChild(boxAuthor);
+  boxDiv.appendChild(boxPages);
+  boxDiv.appendChild(boxRead);
+  }
 }
 
 bookSubmit.addEventListener('click', (e) => {
+  addBookToLib();
   createBookBox();
   e.preventDefault();
 })
